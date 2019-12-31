e47aa8664c014e21b65e274d00d89cbb668a8edf
song <740341854@qq.com>
2019-12-31 17:33:47 +0800

    基础页面布局, 添加npm install bootstrap, laravel6默认没有bootstrap

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由<a href="#" target="_blank">Song</a>设计和编码<span style=""></span>
+        </p>
+
+        <p class="float-right"><a href="mailto:470341854@qq.com">联系我们</a></p>
+    </div>
+</footer>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- css -->
+    <link rel="stylesheet" href="{{ config('app.subdir').mix('css/app.css') }}">
+
+    <title>@yield('title', 'LaraBBS') - 进阶</title>
+</head>
+<body>
+    <div id="app" class="{{ route_class() }}-page">
+    @include('layouts._header')
+    <div class="container">
+        @include('shared._messages')
+        @yield('content')
+    </div>
+    @include('layouts._footer')
+    </div>
+
+    <!-- scripts -->
+    <script src="{{ config('app.subdir').mix('js/app.js') }}"></script>
+</body>
+</html>
