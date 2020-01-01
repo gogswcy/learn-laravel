af80539b5e82f9facfcb7a42cbc5dae39b68fc3f
song <740341854@qq.com>
2020-01-01 13:30:08 +0800

    修改跳转链接

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -15,8 +15,29 @@
         <!-- Right Side Of Navbar -->
         <ul class="navbar-nav navbar-right">
         <!-- Authentication Links -->
+            @guest
             <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">登录</a></li>
             <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">注册</a></li>
+            @else
+            <li class="nav-item dropdown">
+                <a href="#" role="button" class="nav-link dropdown-toggle" id="navbarDropdown" data-toggle="dropdown">
+                    <img src="https://laravel-china.org/uploads/images/201709/20/1/PtDKbASVcz.png" style="width: 30px; height: 30px;" alt="">
+                    {{ Auth::user()->name }}
+                </a>
+
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a href="" class="dropdown-item">个人中心</a>
+                    <a href="" class="dropdown-item">编辑资料</a>
+                    <div class="dropdown-divider"></div>
+                    <a href="#" class="dropdown-item" id="logout">
+                        <form action="{{ route('logout') }}" method="POST">
+                            {{ csrf_field() }}
+                            <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                        </form>
+                    </a>
+                </div>
+            </li>
+            @endguest
         </ul>
     </div>
     </div>
