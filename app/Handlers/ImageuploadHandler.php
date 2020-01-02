2120b764db6caf62355f2ea1651b476b8fe80fb2
song <740341854@qq.com>
2020-01-02 18:40:41 +0800

    裁减图像

--- app/Handlers/ImageuploadHandler.php
+++ app/Handlers/ImageuploadHandler.php
@@ -1,12 +1,13 @@
 <?php
 namespace App\Handlers;
+use Image;
 
 class ImageuploadHandler
 {
     // 允许后缀
     protected $allow_ext = ['png', 'jpg', 'gif', 'jpeg'];
 
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, max_width = false)
     {
         // 文件的切割让查找更有效率
         $folder_name = "uploads/images/$folder/" . date('Ym/d', time());
@@ -25,8 +26,32 @@ class ImageuploadHandler
 
         $file->move($upload_path, $filename);
 
+        // 裁减
+        if ($max_width && $extension != 'gif') {
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
+
         return [
             'path' => "/$folder_name/$filename"
         ];
+    }
+
+    /**
+     * 裁减图片
+     */
+    public function reduceSize($file_path, $max_width)
+    {
+        // 传参, 磁盘路径
+        $image = Image::make($file_path);
+
+        // 进行大小的调整
+        $image->resize($max_width, null, function ($constrait) {
+            // 等比例缩放
+            $constrait->aspectRatio();
+            // 防止图片尺寸变大
+            $constrait->upsize();
+        });
+        // 保存
+        $image->save();
     }
 }
