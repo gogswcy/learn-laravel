8367398a5f244f2cc28a28fe7d040e45a363fcf0
song <740341854@qq.com>
2020-01-03 11:47:04 +0800

    填充用户和话题数据

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -1,20 +1,28 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
+use App\Models\User;
 
 class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
-        });
+        // 所有用户的id
+        $user_ids = User::all()->pluck('id')->toArray();
+        // 所有分类
+        $category_ids = Category::all()->pluck('id')->toArray();
 
+        $faker = app(Faker\Generator::class);
+
+        $topics = factory(Topic::class)
+            ->times(100)
+            ->make()
+            ->each(function ($topic, $index) use ($user_ids, $category_ids, $faker) {
+                $topic->user_id = $faker->randomElement($user_ids);
+                $topic->category_id = $faker->randomElement($category_ids);
+            });
         Topic::insert($topics->toArray());
     }
-
 }
-
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = app(Faker\Generator::class);
+
+        // 头像
+        $avatars = [
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png',
+        ];
+
+        // 生成数据集合
+        $users = factory(User::class)
+            ->times(10)
+            ->make()
+            ->each(function ($user, $index) use ($faker, $avatars) {
+                $user->avatar = $faker->randomElement($avatars);
+            });
+        
+        // 让隐藏字段可见, 并将数据集合转换为数组
+        $user_arary = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        // 将数据插入到数据库中
+        User::insert($user_arary);
+
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name  = 'Song';
+        $user->email = '740341854@qq.com';
+        $user->avatar = 'https://iocaffcdn.hphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
+    }
+}
