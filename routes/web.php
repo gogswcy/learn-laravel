7a6ab81e97a6f8e2f0a6b2864a561f8ecbd51789
song <740341854@qq.com>
2020-01-03 11:02:41 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,4 @@ Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('ver
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
 route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
